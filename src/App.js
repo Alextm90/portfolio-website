990060c0474c9dff3bcf9565517c7e818bfa3157
Alex Miserandino <amiserandino20@gmail.com>
2023-09-03 23:55:37 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+import "./App.css";
+import About from "./components/about";
+import Navbar from "./components/navbar";
+import Skills from "./components/skills";
+import Projects from "./components/projects"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+  return <div className="App">
+    <Navbar></Navbar>
+    <About></About>
+  </div>;
 }
 
 export default App;
