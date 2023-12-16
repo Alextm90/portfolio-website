009269313a8e249c4bf2cc6d4d35b7ed2552bcc6
Alex Miserandino <amiserandino20@gmail.com>
2023-12-15 23:01:19 -0700

    refactored

--- portfolio-website/src/components/Certificates.jsx
+++ portfolio-website/src/components/Certificates.jsx
@@ -17,36 +17,41 @@ const Certificates = () => {
   ];
 
   const certificateComponents = arr?.map((cert) => {
-   return (
-     <Popup
-       key={cert}
-       trigger={
-         <button className={styles.certBtn}>
-           <img className={styles.certImg} src={cert}></img>
-         </button>
-       }
-       modal
-       nested
-     >
-       {(close) => (
-         <div>
-           <button className={styles.certBtn}>
-             <img className={styles.popup} src={cert}></img>
-           </button>
-           <button onClick={() => close()} className={styles.closeBtn}>
-             <CloseIcon />
-           </button>
-         </div>
-       )}
-     </Popup>
-   );
+    return <Popup
+        key={cert}
+        trigger={
+          <button className={styles.certBtn}>
+            <img className={styles.certImg} src={cert}></img>
+          </button>
+        }
+        modal
+        nested
+      >
+        {(close) => (
+          <div>
+            <button className={styles.certBtn}>
+              <img className={styles.popup} src={cert}></img>
+            </button>
+            <button onClick={() => close()} className={styles.closeBtn}>
+              <CloseIcon />
+            </button>
+          </div>
+        )}
+      </Popup>
   });
-  console.log(certificateComponents[0])
+
   return (
-    <section>
+    <section id={styles.certWrapper} >
       <h1 id={styles.header}>Certificates</h1>
-      {certificateComponents[0]}
-      {certificateComponents[1]}
+      <div id={styles.certContainerOne}>
+        {certificateComponents[0]}
+        {certificateComponents[1]}
+        {certificateComponents[2]}
+      </div>
+      <div id={styles.certContainerTwo}>
+        {certificateComponents[3]}
+        {certificateComponents[4]}
+      </div>
     </section>
   );
 };
