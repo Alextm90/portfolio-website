17d503e90669b514edb5150fe3282d847800cbb0
Alex Miserandino <amiserandino20@gmail.com>
2024-08-28 09:19:07 -0700

    changed size of icons

--- portfolio-website/src/components/Skills.jsx
+++ portfolio-website/src/components/Skills.jsx
@@ -22,52 +22,52 @@ const Skills = () => {
       <h1>Skills</h1>
       <div className={styles.skillContainer}>
         <div className={styles.icon}>
-          <MongodbOriginal size="55" />
+          <MongodbOriginal size="68" />
           <div className={styles.shadow}></div>
           <h4>mongoDB</h4>
         </div>
         <div className={styles.icon}>
-          <ExpressOriginal size="55" fill="gray" />
+          <ExpressOriginal size="68" fill="gray" />
           <div className={styles.shadow}></div>
           <h4>Express</h4>
         </div>
         <div className={styles.icon}>
-          <JavascriptOriginal size="55" />
+          <JavascriptOriginal size="68" />
           <div className={styles.shadow}></div>
           <h4>JavaScript</h4>
         </div>
         <div className={styles.icon}>
-          <PostgresqlOriginal size="55" />
+          <PostgresqlOriginal size="68" />
           <div className={styles.shadow}></div>
           <h4>PostgreSQL</h4>
         </div>
         <div className={styles.icon}>
-          <NodejsOriginal size="55" />
+          <NodejsOriginal size="68" />
           <div className={styles.shadow}></div>
           <h4>Node.js</h4>
         </div>
         <div className={styles.icon}>
-          <Html5Original size="55" />
+          <Html5Original size="68" />
           <div className={styles.shadow}></div>
           <h4>HTML5</h4>
         </div>
         <div className={styles.icon}>
-          <ReactOriginal size="55" />
+          <ReactOriginal size="68" />
           <div className={styles.shadow}></div>
           <h4>React</h4>
         </div>
         <div className={styles.icon}>
-          <Css3Original size="55" fill="gray" />
+          <Css3Original size="68" fill="gray" />
           <div className={styles.shadow}></div>
           <h4>CSS3</h4>
         </div>
         <div className={styles.icon}>
-          <D3jsOriginal size="55" />
+          <D3jsOriginal size="68" />
           <div className={styles.shadow}></div>
           <h4>D3.js</h4>
         </div>
         <div className={styles.icon}>
-          <MochaPlain size="55" />
+          <MochaPlain size="68" />
           <div className={styles.shadow}></div>
           <h4>Mocha</h4>
         </div>
@@ -75,22 +75,22 @@ const Skills = () => {
       <h1 id={styles.skills}>Tech</h1>
       <div className={styles.skillContainer}>
         <div className={styles.icon}>
-          <GitOriginal size="55" />
+          <GitOriginal size="68" />
           <div className={styles.shadow}></div>
           <h4>Git</h4>
         </div>
         <div className={styles.icon}>
-          <VscodePlainWordmark size="55" fill="gray" />
+          <VscodePlainWordmark size="68" fill="gray" />
           <div className={styles.shadow}></div>
           <h4>Vscode</h4>
         </div>
         <div className={styles.icon}>
-          <BashOriginal size="55" />
+          <BashOriginal size="68" />
           <div className={styles.shadow}></div>
           <h4>Bash</h4>
         </div>
         <div className={styles.icon}>
-          <GithubOriginal size="55" color="red" />
+          <GithubOriginal size="68" color="red" />
           <div className={styles.shadow}></div>
           <h4>GitHub</h4>
         </div>
