8a82fb63f10c40cafe08ba6b03cf53c27db500af
Alex Miserandino <amiserandino20@gmail.com>
2024-08-28 09:25:55 -0700

    added classnames for styling + added certificate link

--- portfolio-website/src/components/Navbar.jsx
+++ portfolio-website/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import styles from "./Navbar.module.css";
-import { useState } from "react";
-import ReorderIcon from "@mui/icons-material/Reorder";
-import CloseIcon from "@mui/icons-material/Close";
+import styles from './Navbar.module.css';
+import { useState } from 'react';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import CloseIcon from '@mui/icons-material/Close';
 
 function Navbar() {
   // control navbar
@@ -14,7 +14,7 @@ function Navbar() {
   return (
     <div>
       <div className={styles.navbar} id={navbar ? styles.open : styles.close}>
-        <p>Alex Miserandino</p>
+        <p className={styles.navLinks}>Alex Miserandino</p>
         <div className={styles.menu}>
           <button onClick={handleClick}>
             {navbar ? <CloseIcon /> : <ReorderIcon />}
@@ -22,32 +22,48 @@ function Navbar() {
         </div>
         <ul className={styles.nav}>
           <li>
-            <a href="#skills">Skills</a>
+            <a href="#skills" className={styles.navLinks}>
+              Skills
+            </a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a href="#projects" className={styles.navLinks}>
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a href="#certificates" className={styles.navLinks}>
+              Certificates
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a href="#contact" className={styles.navLinks}>
+              Contact
+            </a>
           </li>
         </ul>
       </div>
-      <div id={navbar ? styles.menuContainerOpen: styles.menuContainerClose}>
+      <div id={navbar ? styles.menuContainerOpen : styles.menuContainerClose}>
         <ul onClick={handleClick}>
           <li>
-            <a href="#skills">Skills</a>
+            <a href="#skills" className={styles.navLinks}>
+              Skills
+            </a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a href="#projects" className={styles.navLinks}>
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a href="#certificates" className={styles.navLinks}>
+              Certificates
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a href="#contact" className={styles.navLinks}>
+              Contact
+            </a>
           </li>
         </ul>
       </div>
@@ -56,4 +72,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
