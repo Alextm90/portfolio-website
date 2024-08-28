5c105ecc963e5d35cc20bd1646c176126c5c6551
Alex Miserandino <amiserandino20@gmail.com>
2024-08-28 09:30:16 -0700

    added two sections

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,20 +1,24 @@
-import "./App.css";
-import About from "./components/about";
-import Navbar from "./components/navbar";
-import Skills from "./components/skills";
-import Projects from "./components/projects";
-import { ParallaxProvider } from "react-scroll-parallax";
-import Certificates from "./components/Certificates";
+import './App.css';
+import About from './components/About';
+import Navbar from './components/navbar';
+import Skills from './components/skills';
+import Projects from './components/projects';
+import { ParallaxProvider } from 'react-scroll-parallax';
+import Certificates from './components/Certificates';
+import ContactForm from './components/ContactForm';
+import PortfolioHeader from './components/PortfolioHeader';
 
 function App() {
   return (
     <div className="App">
       <ParallaxProvider>
         <Navbar></Navbar>
+        <PortfolioHeader></PortfolioHeader>
         <Skills></Skills>
         <Projects></Projects>
         <Certificates></Certificates>
         <About></About>
+        <ContactForm></ContactForm>
       </ParallaxProvider>
     </div>
   );
