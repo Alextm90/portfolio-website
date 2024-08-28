6698f85bdcc9b883fe6d4827f9da6447c3963642
Alex Miserandino <amiserandino20@gmail.com>
2024-08-28 09:36:04 -0700

    shortened text length

--- portfolio-website/src/components/text.jsx
+++ portfolio-website/src/components/text.jsx
@@ -5,16 +5,12 @@ const textObject = {
             provident porro libero commodi consequatur! Lorem ipsum dolor sit
             amet, consectetur adipisicing elit. Nihil, reiciendis? Doloremque
             reiciendis maiores deserunt voluptas quod adipisci ipsa illo
-            mollitia vitae! Magni quidem consectetur nam, provident porro libero
-            commodi consequatur!`,
+            mollitia vitae!`,
   descriptionTwo: `Porum ipsum dolor sit amet, consectetur adipisicing elit. Nihil,
             reiciendis? Doloremque reiciendis maiores deserunt voluptas quod
             adipisci ipsa illo mollitia vitae! Magni quidem consectetur nam,
             provident porro libero commodi consequatur! Lorem ipsum dolor sit
-            amet, consectetur adipisicing elit. Nihil, reiciendis? Doloremque
-            reiciendis maiores deserunt voluptas quod adipisci ipsa illo
-            mollitia vitae! Magni quidem consectetur nam, provident porro libero
-            commodi consequatur!`,
+            amet, consectetur adipisicing elit. Nihil, reiciendis?`,
   descriptionThree: `dolor sit amet, consectetur adipisicing elit. Nihil,
             reiciendis? Doloremque reiciendis maiores deserunt voluptas quod
             adipisci ipsa illo mollitia vitae! Magni quidem consectetur nam,
