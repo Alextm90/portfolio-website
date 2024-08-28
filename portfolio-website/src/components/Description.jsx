43f1265d6cdd6224aeff5b95757b3d508680a4f8
Alex Miserandino <amiserandino20@gmail.com>
2024-08-28 09:23:55 -0700

    added  ViewAppLink component + props

--- portfolio-website/src/components/Description.jsx
+++ portfolio-website/src/components/Description.jsx
@@ -1,10 +1,32 @@
-import styles from "./projects.module.css";
-import { GithubOriginal } from "devicons-react";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import { Parallax } from "react-scroll-parallax";
+import { GithubOriginal } from 'devicons-react';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import { Parallax } from 'react-scroll-parallax';
+import styles from './projects.module.css';
 
-const Description = ({ description, title, arr }) => {
+const ViewAppLink = ({ webLnk }) => {
+  return (
+    <a
+      href={webLnk}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={styles.lnk}
+    >
+      <span className={styles.eyeLnkIcon}>
+        <VisibilityIcon style={{ fontSize: '15' }} />
+      </span>
+      <span className={styles.eyeLnkText}>Live App</span>
+    </a>
+  );
+};
 
+const Description = ({
+  description,
+  title,
+  arr,
+  showViewAppLink,
+  webLnk,
+  codeLnk,
+}) => {
   return (
     <Parallax speed={-7}>
       <div className={styles.descriptionContainer}>
@@ -21,19 +43,19 @@ const Description = ({ description, title, arr }) => {
           </div>
           <p id={styles.description}>{description}</p>
         </div>
-        <div className={styles.btnContainer}>
-          <button className={styles.btn}>
-            <span className={styles.eyeBtnIcon}>
-              <VisibilityIcon style={{ fontSize: "small" }} />
+        <div className={styles.lnkContainer}>
+          {showViewAppLink && <ViewAppLink webLnk={webLnk} />}
+          <a
+            className={styles.lnk}
+            href={codeLnk}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className={styles.lnkIcon}>
+              <GithubOriginal size="13" fill="gray" />
             </span>
-            <span className={styles.eyeBtnText}>Live App</span>
-          </button>
-          <button className={styles.btn}>
-            <span className={styles.btnIcon}>
-              <GithubOriginal size="10" fill="gray" />
-            </span>
-            <span className={styles.btnText}>View Code</span>
-          </button>
+            <span className={styles.lnkText}>View Code</span>
+          </a>
         </div>
       </div>
     </Parallax>
