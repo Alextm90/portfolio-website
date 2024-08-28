4119608f4a29be55290e376334a8fe405066c636
Alex Miserandino <amiserandino20@gmail.com>
2024-08-28 09:22:00 -0700

    added links to be passed to Description

--- portfolio-website/src/components/Projects.jsx
+++ portfolio-website/src/components/Projects.jsx
@@ -1,45 +1,66 @@
-import styles from "./projects.module.css";
-import project1 from "../assets/project1.png";
-import project2 from "../assets/project2.png";
-import Description from "./description";
-import textObject from "./text";
+import styles from './projects.module.css';
+import project1 from '../assets/project1.png';
+import project12 from '../assets/project12.png';
+import project8 from '../assets/project9.png';
+import Description from './Description';
+import textObject from './text';
 
 const Projects = () => {
-  const { descriptionOne, descriptionTwo, descriptionThree, descriptionFour } = textObject;
-  
-  const arr = ["one", "two", "three", "four"]
-  const arrTwo = ["one", "two", "three"];
+  const { descriptionOne, descriptionTwo, descriptionThree, descriptionFour } =
+    textObject;
+
+  const arr = ['one', 'two', 'three', 'four'];
+  const arrTwo = ['JavaScript', 'D3.js'];
+  const arrThree = ['one', 'two', 'three', 'four', 'five'];
 
   return (
     <section id={styles.projects}>
-      <h1>Projects</h1>
+      <h1 id="projects" style={{ marginBottom: '2rem' }}>Projects</h1>
       <div
         className={styles.projectContainer}
         style={{
-          backgroundImage: `url(${project1})`,
+          backgroundImage: `url(${project8})`,
         }}
       >
         <Description
           title="Issue Tracker"
           description={descriptionOne}
-          arr={arr}
+          arr={arrTwo}
+          showViewAppLink={true}
+          webLnk="https://my-frontend-jnhl.onrender.com/"
+          codeLnk="https://github.com/Alextm90/issue-tracker"
         />
       </div>
       <div
         className={styles.projectContainer}
         style={{
-          backgroundImage: `url(${project2})`,
+          backgroundImage: `url(${project12})`,
         }}
       >
-        <Description title="Heat Map" description={descriptionTwo} arr={arrTwo} />
+        <div id={styles.project12}>
+          <Description
+            title="Number Guessing Game"
+            description={descriptionTwo}
+            arr={arr}
+            showViewAppLink={false}
+            codeLnk="https://github.com/Alextm90/number-guessing-game"
+          />
+        </div>
       </div>
       <div
         className={styles.projectContainer}
         style={{
-          backgroundImage: `url(${project1})`,
+          backgroundImage: `linear-gradient(rgba(0, 0, 255, 0.33), rgba(0, 0, 255, 0.33)), url(${project1})`,
         }}
       >
-        <Description title="Timer" description={descriptionThree} />
+        <Description
+          title="Timer"
+          description={descriptionTwo}
+          showViewAppLink={true}
+          webLnk="https://alextm-25-5-clock.surge.sh/"
+          codeLnk="https://github.com/Alextm90/pomodoro-timer"
+          arr={arrThree}
+        />
       </div>
     </section>
   );
