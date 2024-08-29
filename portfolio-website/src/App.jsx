4c1b47266d4bee4c463491dc84d003e3636723c5
Alex Miserandino <amiserandino20@gmail.com>
2024-08-29 10:10:34 -0700

    added footer component

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -7,6 +7,7 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 import Certificates from './components/Certificates';
 import ContactForm from './components/ContactForm';
 import PortfolioHeader from './components/PortfolioHeader';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Certificates></Certificates>
         <About></About>
         <ContactForm></ContactForm>
+        <Footer></Footer>
       </ParallaxProvider>
     </div>
   );
