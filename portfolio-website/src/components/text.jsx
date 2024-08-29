0dacc9e56f2b5e6265d07f2146a66fbfdef872fe
Alex Miserandino <amiserandino20@gmail.com>
2024-08-29 10:13:53 -0700

    filled in description

--- portfolio-website/src/components/text.jsx
+++ portfolio-website/src/components/text.jsx
@@ -1,24 +1,7 @@
 const textObject = {
-  descriptionOne: `Forum ipsum dolor sit amet, consectetur adipisicing elit. Nihil,
-            reiciendis? Doloremque reiciendis maiores deserunt voluptas quod
-            adipisci ipsa illo mollitia vitae! Magni quidem consectetur nam,
-            provident porro libero commodi consequatur! Lorem ipsum dolor sit
-            amet, consectetur adipisicing elit. Nihil, reiciendis? Doloremque
-            reiciendis maiores deserunt voluptas quod adipisci ipsa illo
-            mollitia vitae!`,
-  descriptionTwo: `Porum ipsum dolor sit amet, consectetur adipisicing elit. Nihil,
-            reiciendis? Doloremque reiciendis maiores deserunt voluptas quod
-            adipisci ipsa illo mollitia vitae! Magni quidem consectetur nam,
-            provident porro libero commodi consequatur! Lorem ipsum dolor sit
-            amet, consectetur adipisicing elit. Nihil, reiciendis?`,
-  descriptionThree: `dolor sit amet, consectetur adipisicing elit. Nihil,
-            reiciendis? Doloremque reiciendis maiores deserunt voluptas quod
-            adipisci ipsa illo mollitia vitae! Magni quidem consectetur nam,
-            provident porro libero commodi consequatur! Lorem ipsum dolor sit
-            amet, consectetur adipisicing elit. Nihil, reiciendis? Doloremque
-            reiciendis maiores deserunt voluptas quod adipisci ipsa illo
-            mollitia vitae! Magni quidem consectetur nam, provident porro libero
-            commodi consequatur!`,
+  descriptionOne: `A MERN stack app that manages issues with CRUD operations and is deployed using Docker. The project features an authentication system with access and refresh tokens, using Axios interceptors to maintain user authentication. It securely manages sessions by utilizing refresh tokens to maintain user access and protect routes for authenticated users. The backend is tested with Mocha, Chai, and Axios to ensure correct functionality, including user registration and issue management.`,
+  descriptionTwo: `A terminal-based number guessing game allows players to guess a random number between 1 and 1,000. The number of games played and the best score are stored and retrieved from a PostgreSQL database. Each session is tailored based on the player's history, creating a unique experience. The game is managed through a Bash script that interacts with the database to track and update player progress.`,
+  descriptionThree: `This is a Pomodoro timer built with React. The timer features customizable session and break lengths, and transitions automatically between work and break intervals. It also includes audio alerts to signal the start and end of each session or break. The primary purpose of this project was to learn React and state management.`,
   descriptionFour: `ipsum dolor sit amet, consectetur adipisicing elit. Nihil,
             reiciendis? Doloremque reiciendis maiores deserunt voluptas quod
             adipisci ipsa illo mollitia vitae! Magni quidem consectetur nam,
@@ -29,4 +12,4 @@ const textObject = {
             commodi consequatur!`,
 };
 
-export default textObject
+export default textObject;
