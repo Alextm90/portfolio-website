856e66baf88d25ef7dbaf902197fcc852b2cc756
Alex Miserandino <amiserandino20@gmail.com>
2024-09-20 22:48:49 -0400

    removed icon size

--- portfolio-website/src/components/Footer.jsx
+++ portfolio-website/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import {ReactOriginal} from "devicons-react";
 const Footer = () => {
   return (
     <div className={styles.footerContainer}>
-      <p className={styles.footerText}>Website Made with React <span><ReactOriginal size="15" /></span> by Alex Miserandino</p>
+      <p className={styles.footerText}>Website Made with React <span><ReactOriginal /></span> by Alex Miserandino</p>
     </div>
   );
 }
